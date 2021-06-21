cc6c759ef72603b423db20a588084f4a42a49d55
ElijahAA <oeeagu@gmail.com>
2021-06-21 16:06:06 +1000

    Implemented LCU Webhook connector.

--- models/league-session.js
+++ models/league-session.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const ws = require("ws");
 
 
 module.exports = class LeagueSession {
@@ -9,12 +10,13 @@ module.exports = class LeagueSession {
 		this.username = username;
 		this.password = password;
 		this.authentication = new Buffer(`${username}:${password}`).toString('base64');
+		this.listen();
 	}
 
 	async get(path, params = {}) {
 		try {
-			return await axios.get(`${self.protocol}://${self.address}:${self.port}${path}`, {
-				headers: {"Authorization": `Basic ${self.authentication}`},
+			return await axios.get(`${this.protocol}://${this.address}:${this.port}${path}`, {
+				headers: {"Authorization": `Basic ${this.authentication}`},
 				params: params
 			})
 		} catch (e) {
@@ -24,8 +26,8 @@ module.exports = class LeagueSession {
 
 	async post(path, params = {}) {
 		try {
-			return await axios.post(`${self.protocol}://127.0.0.1:${self.port}${path}`, {
-				headers: {"Authorization": `Basic ${self.authentication}`},
+			return await axios.post(`${this.protocol}://127.0.0.1:${this.port}${path}`, {
+				headers: {"Authorization": `Basic ${this.authentication}`},
 				params: params
 			})
 		} catch (e) {
@@ -35,8 +37,8 @@ module.exports = class LeagueSession {
 
 	async put(path, params = {}) {
 		try {
-			return await axios.put(`${self.protocol}://127.0.0.1:${self.port}${path}`, {
-				headers: {"Authorization": `Basic ${self.authentication}`},
+			return await axios.put(`${this.protocol}://127.0.0.1:${this.port}${path}`, {
+				headers: {"Authorization": `Basic ${this.authentication}`},
 				params: params
 			})
 		} catch (e) {
@@ -46,8 +48,8 @@ module.exports = class LeagueSession {
 
 	async patch(path, params = {}) {
 		try {
-			return await axios.patch(`${self.protocol}://127.0.0.1:${self.port}${path}`, {
-				headers: {"Authorization": `Basic ${self.authentication}`},
+			return await axios.patch(`${this.protocol}://127.0.0.1:${this.port}${path}`, {
+				headers: {"Authorization": `Basic ${this.authentication}`},
 				params: params
 			})
 		} catch (e) {
@@ -57,13 +59,29 @@ module.exports = class LeagueSession {
 
 	async head(path, params = {}) {
 		try {
-			return await axios.head(`${self.protocol}://127.0.0.1:${self.port}${path}`, {
-				headers: {"Authorization": `Basic ${self.authentication}`},
+			return await axios.head(`${this.protocol}://127.0.0.1:${this.port}${path}`, {
+				headers: {"Authorization": `Basic ${this.authentication}`},
 				params: params
 			})
 		} catch (e) {
 			throw e;
 		}
+	}
+
+	listen() {
+		console.log("Attempting to listen.")
+		//Sketchy, Hopefully fix properly in future.
+		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+		let listener = new ws(`https://riot:${this.password}@127.0.0.1:${this.port}`, 'wamp', {});
+
+		listener.on('open', () => {
+			listener.send('[5, "OnJsonApiEvent"]')
+		})
+
+		listener.on("message", (data) => {
+			console.log(data)
+			console.log((JSON.parse(data.toString())));
+		})
 	}
 
 }
